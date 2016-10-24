e5866b34f868e3ed9d9540756300a559dd24d906
Vincent Touchard <vincentoo@yahoo.com>
2016-10-24 21:05:53 +0200

    Enforce events to have an associated device and a timestamp

--- lib/rzwaveway/events.rb
+++ lib/rzwaveway/events.rb
@@ -1,6 +1,16 @@
 require 'ostruct'
 
 module RZWaveWay
+  class Event < OpenStruct
+    def initialize(hash)
+      raise ArgumentError, 'Hash can not be nil' unless hash
+      raise ArgumentError, 'Missing device_id' unless hash.has_key? :device_id
+      hash[:time] = Time.now unless hash.has_key? :time
+
+      super(hash)
+    end
+  end
+
   class AlarmEvent < OpenStruct ; end
 
   class AliveEvent < OpenStruct ; end
---
+++ spec/events_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+module RZWaveWay
+  describe Event do
+    describe '#new' do
+      it 'enforces a non-nil argument' do
+        expect { Event.new(nil) }
+        .to raise_error ArgumentError
+      end
+
+      it 'requires a time' do
+        expect { Event.new(foo: 'bar') }
+        .to raise_error ArgumentError 
+      end
+
+      it 'adds a time if none is provided' do
+        event = Event.new(device_id: 123)
+        expect(event[:time]).not_to be_nil
+      end
+
+      it 'keeos the existing time if provided' do
+        event = Event.new(device_id: 123, time: 1472373723)
+        expect(event[:time]).to eq 1472373723
+      end
+    end    
+  end
+end
