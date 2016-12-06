426b770e1c5613e72027fae7971d4a8af1d3783b
Vincent Touchard <vincentoo@yahoo.com>
2016-12-06 23:43:16 +0100

    Complete the alarm test.
    It was not doing much... yet. Now at least it checks something.

--- spec/command_classes/alarm_spec.rb
+++ spec/command_classes/alarm_spec.rb
@@ -4,7 +4,7 @@ module RZWaveWay
   module CommandClasses
     describe Alarm do
       let(:device) { ZWaveDevice.new(create_id, create_device_data) }
-      let(:command_class) { Alarm.new(device) }
+      let(:command_class) { Alarm.new({}, device) }
 
       describe '#process' do
         it 'returns an alarm event' do
@@ -12,7 +12,10 @@ module RZWaveWay
           updates = JSON.parse updates
           zway = ZWay.instance
           updates_per_device = zway.send(:group_per_device, updates)
-          device.process(updates_per_device[12])
+          updates_per_cc = device.send(:group_per_commandclass, updates_per_device[12])
+          event = command_class.process(updates_per_cc[113])
+          expect(event.alarm_type).to eq 8
+          expect(event.level).to eq 2
         end
       end
     end
