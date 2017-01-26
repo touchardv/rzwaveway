e661bf1660f5603f84a8ef87d4850f0f18c5a4dd
Vincent Touchard <vincentoo@yahoo.com>
2017-01-26 19:57:01 +0100

    Add alarm types.

--- lib/rzwaveway/command_classes/alarm.rb
+++ lib/rzwaveway/command_classes/alarm.rb
@@ -2,6 +2,19 @@ module RZWaveWay
   module CommandClasses
     class Alarm < CommandClass
 
+      # from zwayDev.pdf
+      TYPE_SMOKE = 0x01
+      TYPE_CO    = 0x02
+      TYPE_CO2   = 0x03
+      TYPE_HEAT  = 0x04
+      TYPE_WATER = 0x05
+      TYPE_ACCESS_CONTROL = 0x06
+      TYPE_BURGLAR = 0x07
+      TYPE_POWER_MANAGEMENT = 0x08
+      TYPE_SYSTEM = 0x09
+      TYPE_EMERGENCY = 0x0a
+      TYPE_CLOCK = 0x0b
+
       def property_mappings
         {}
       end
